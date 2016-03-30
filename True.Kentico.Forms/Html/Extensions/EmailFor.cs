8847725edee00d04e994526868fc9ff30a32687c
Mark Calder <mark.calder@truedigital.co.uk>
2016-03-30 13:25:05 +0100

    remove former version of method

--- True.Kentico.Forms/Html/Extensions/EmailFor.cs
+++ True.Kentico.Forms/Html/Extensions/EmailFor.cs
@@ -55,53 +55,5 @@ namespace True.Kentico.Forms.Html.Extensions
 
             return MvcHtmlString.Create(div.ToString());
         }
-
-        public static IHtmlString EmailFor<TModel, TProperty>(this KenticoForm<TModel> html, Expression<Func<TModel, TProperty>> expression)
-        {
-            var item = (MemberExpression)expression.Body;
-            var id = item.Member.Name;
-
-            var dispAttr = GetAttribute<DisplayAttribute>(item);
-            var displayName = dispAttr != null ? dispAttr.Name : item.Member.Name;
-
-            var reqAttr = GetAttribute<RequiredAttribute>(item);
-            var helpTextAttr = GetAttribute<HelpTextAttribute>(item);
-            var equalTo = GetAttribute<CompareAttribute>(item);
-
-            var div = new MultiLevelTag("div");
-            div.AddCssClass("form-inner");
-
-            var input = new MultiLevelTag("input");
-            input.Attributes.Add("id", id);
-            input.Attributes.Add("name", id);
-            input.Attributes.Add("type", "email");
-
-            if (equalTo != null)
-            {
-                input.Attributes.Add("equalTo", $"#{equalTo.OtherProperty}");
-            }
-
-            if (reqAttr != null)
-            {
-                input.Attributes.Add("required", null);
-                input.Attributes.Add("data-msg-required", $"{displayName} is required");
-            }
-
-            input.Attributes.Add("data-msg-email", $"{displayName} is invalid");
-
-            input.Attributes.Add("data-msg-equalto", $"Does not match");
-
-            div.Add(input);
-
-            if (helpTextAttr != null)
-            {
-                var helpTextDiv = new MultiLevelTag("div");
-                helpTextDiv.AddCssClass("form-help");
-                helpTextDiv.InnerHtml = helpTextAttr.HelpText;
-                div.Add(helpTextDiv);
-            }
-
-            return MvcHtmlString.Create(div.ToString());
-        }
     }
 }
