b0fd69d06642ce956c4be011cfea34f7f6a88ce4
Mark Calder <mark.calder@truedigital.co.uk>
2016-03-31 11:38:54 +0100

    add controller to beginform

--- True.Kentico.Forms/Html/Extensions/BeginForm.cs
+++ True.Kentico.Forms/Html/Extensions/BeginForm.cs
@@ -5,9 +5,9 @@ namespace True.Kentico.Forms.Html.Extensions
 {
     public static partial class KenticoFormHelperExtensions
     {
-        public static IHtmlString BeginForm<TModel>(this KenticoForm<TModel> html, string action = "", string method = "post", string dataLayout = "inline", bool validate = true, bool summary = true)
+        public static IHtmlString BeginForm<TModel>(this KenticoForm<TModel> html, string action = "", string controllerName = "", string method = "post", string dataLayout = "inline", bool validate = true, bool summary = true)
         {
-            return MvcHtmlString.Create($"<form method=\"{method}\" action=\"{action}\" class=\"form\"{(validate ? " data-validate" : string.Empty)}{(summary ? " data-summary" : string.Empty)} data-layout=\"{dataLayout}\">");
+            return MvcHtmlString.Create($"<form action=\"/{controllerName}/{action}\" method=\"{method}\" class=\"form\"{(validate ? " data-validate" : string.Empty)}{(summary ? " data-summary" : string.Empty)} data-layout=\"{dataLayout}\">");
         }
 
         public static IHtmlString EndForm<TModel>(this KenticoForm<TModel> html)
