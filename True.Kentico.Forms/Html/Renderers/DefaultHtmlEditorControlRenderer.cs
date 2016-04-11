49cee78b82ce2757f9c534f61bb890da292b732e
gabador <gabor.ferencz@truedigital.co.uk>
2016-04-11 10:20:04 +0100

    added html area renderer

---
+++ True.Kentico.Forms/Html/Renderers/DefaultHtmlEditorControlRenderer.cs
@@ -0,0 +1,33 @@
+﻿using True.Kentico.Forms.Forms.FormParts;
+
+namespace True.Kentico.Forms.Html.Renderers
+{
+    internal class DefaultHtmlEditorControlRenderer : BaseControlRenderer
+    {
+        public override string Render(IControl control)
+        {
+            var id = control.Name;
+
+            var displayName = !string.IsNullOrEmpty(control.Label) ? control.Label : control.Name;
+
+            var div = new MultiLevelTag("div");
+            div.AddCssClass("form-inner");
+
+            var rte = new MultiLevelTag("textarea");
+            rte.Attributes.Add("id", id);
+            rte.Attributes.Add("name", id);
+            rte.AddCssClass("rich-text-editor");
+
+
+            IsRequired(control, rte, displayName);
+
+            div.Add(rte);
+
+            ExplanationText(control, div);
+
+            ToolTip(control, rte);
+
+            return div.ToString();
+        }
+    }
+}
