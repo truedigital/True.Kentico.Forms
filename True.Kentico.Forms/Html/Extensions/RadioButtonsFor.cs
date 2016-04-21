33865ea07d918ce891c60d896551a26695d71e40
Mark Calder <mark.calder@truedigital.co.uk>
2016-04-21 15:28:40 +0100

    overloads

--- True.Kentico.Forms/Html/Extensions/RadioButtonsFor.cs
+++ True.Kentico.Forms/Html/Extensions/RadioButtonsFor.cs
@@ -1,4 +1,6 @@
-﻿using System.Web;
+﻿using System;
+using System.Linq;
+using System.Web;
 using System.Web.Mvc;
 using True.Kentico.Forms.Forms.FormParts;
 using True.Kentico.Forms.Html.Renderers;
@@ -6,20 +8,36 @@ using True.Kentico.Forms.Infrastructure;
 
 namespace True.Kentico.Forms.Html.Extensions
 {
-	public static partial class KenticoFormHelperExtensions
-	{
-	    public static IHtmlString RadioButtonFor<TControl>(this KenticoForm html, TControl control) where TControl : IControl
-	        
-	    {
-	        return RadioButtonFor<TControl>(html, control, ControlRendererRegistrar.Resolve(ControlType.RadioButton));
-	    }
+    public static partial class KenticoFormHelperExtensions
+    {
+        public static IHtmlString RadioButtonFor<TControl>(this KenticoForm html, TControl control) where TControl : IControl
 
-	    public static IHtmlString RadioButtonFor<TControl>(this KenticoForm html, TControl control, IControlRenderer customRenderer) where TControl : IControl
-            
         {
-             var renderedControl = customRenderer.Render(control);
+            return RadioButtonFor<TControl>(html, control, ControlRendererRegistrar.Resolve(ControlType.RadioButton));
+        }
+
+        public static IHtmlString RadioButtonFor<TControl>(this KenticoForm html, TControl control, IControlRenderer customRenderer) where TControl : IControl
+
+        {
+            var renderedControl = customRenderer.Render(control);
 
             return MvcHtmlString.Create(renderedControl.ToString());
-        }        
-	}
+        }
+
+        public static IHtmlString RadioButtonFor(this KenticoForm html, IForm model, string controlName)
+        {
+            var control = model.Controls.FirstOrDefault(ctrl => ctrl.Name.Equals(controlName, StringComparison.OrdinalIgnoreCase));
+
+            return control != null ?
+                CalendarFor(html, control, ControlRendererRegistrar.Resolve(ControlType.RadioButton))
+                : MvcHtmlString.Create("");
+        }
+
+        public static IHtmlString RadioButtonFor(this KenticoForm html, IForm model, string controlName, IControlRenderer customRenderer)
+        {
+            var control = model.Controls.FirstOrDefault(ctrl => ctrl.Name.Equals(controlName, StringComparison.OrdinalIgnoreCase));
+            var renderedControl = customRenderer.Render(control);
+            return MvcHtmlString.Create(renderedControl);
+        }
+    }
 }
